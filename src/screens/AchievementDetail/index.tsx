e087da05eebbd9f4a3bdbc201cd23dfebf7725b5
lucasoliveira1070 <loliveira306@yahoo.com>
2022-09-27 22:05:31 -0300

    Setup achievement detail screen

--- src/screens/AchievementDetail/index.tsx
+++ src/screens/AchievementDetail/index.tsx
@@ -1,13 +1,62 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
+import { Button, ProgressBar } from 'react-native-paper';
 import React, { useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "./styles";
+import { IconButton } from "react-native-paper";
 
 export function AchievementDetail() {
     const navigation = useNavigation<any>();
+
+    function handleBack() {
+        navigation.goBack();
+    }
+
     return (
         <View style={styles.mainView}>
+            <View style={styles.backButtonView}>
+                <IconButton
+                    icon="arrow-left"
+                    size={26}
+                    style={styles.backIcon}
+                    onPress={handleBack}
+                    iconColor='white'
+                />
+            </View>
+            <Image style={styles.image} source={require("../../../assets/100km-icon.jpg")}></Image>
+            <Text style={styles.title}>First 100 KM</Text>
+            <Text style={styles.dateAcquired}>02/03/2022</Text>
+            <Button icon='share' style={styles.shareButton} buttonColor="#FFAB1E" mode="contained">Share</Button>
+            <View style={styles.cardTitle}>
+                <Text style={styles.description}>Description</Text>
+            </View>
+            <View style={styles.descriptionCardView}>
+                <View style={{ flexDirection: 'row', flex: 1 }}>
+                    <Text style={styles.descriptionCardText}>Drive 100 Kilometers</Text>
+                    <IconButton
+                        style={styles.checkIcon}
+                        icon="check-circle"
+                        iconColor='green'
+                        size={30}
+                    />
+                </View>
+            </View>
 
+            <View style={styles.cardTitle}>
+                <Text style={styles.description}>Next Step</Text>
+            </View>
+            <View style={styles.nextStepCardView}>
+                <View style={{ flexDirection: 'row', flex: 1 }}>
+                    <Text style={styles.nextStepText}>Drive 200 Kilometers</Text>
+                    <IconButton
+                        style={styles.checkIcon}
+                        icon="check-circle"
+                        iconColor='green'
+                        size={30}
+                    />
+                </View>
+                <ProgressBar style={styles.progressBar} progress={0.40} color='#FFAB1E'/>
+            </View>
         </View>
     )
 }
--- src/screens/AchievementDetail/styles.ts
+++ src/screens/AchievementDetail/styles.ts
@@ -1,9 +1,92 @@
 import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
-    mainView:{
-        alignContent:'center',
-        justifyContent:'center',
-        backgroundColor:'#1A1A1A',
-        height:'100%'
+    mainView: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1A1A1A',
+        flex: 1,
+        textAlign: 'center',
+    },
+    image: {
+        justifyContent: 'center',
+        borderRadius: 50,
+        height: 170,
+        width: 175,
+
+    },
+    dateAcquired: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 15,
+        textAlign: 'center',
+    },
+    title: {
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 40,
+    },
+    backIcon: {
+        backgroundColor: '#333333',
+        borderRadius: 12,
+
+    },
+    backButtonView: {
+        width: '100%'
+    },
+    description: {
+        color: '#7F7A7A',
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    descriptionCardView: {
+        width: '80%',
+        backgroundColor: '#333333',
+        height: '10%',
+        paddingVertical: 10,
+        borderRadius: 20,
+        borderColor: 'green',
+        borderWidth: 1,
+        marginBottom: '10%'
+    },
+    descriptionCardText: {
+        color: 'white',
+        fontSize: 18,
+        marginVertical: '5%',
+        marginLeft: '5%',
+        fontWeight: 'bold'
+    },
+    checkIcon: {
+        marginLeft: '23%'
+    },
+    cardTitle: {
+        width: '100%',
+        marginLeft: '25%'
+    },
+    progressBar: {
+        width: '60%',
+        marginHorizontal: '5%'
+    },
+    nextStepText: {
+        color: 'white',
+        fontSize: 18,
+        marginLeft: '5%',
+        marginTop: '2%',
+        fontWeight: 'bold'
+    },
+    shareButton: {
+        marginBottom: '10%'
+    },
+    nextStepCardView: {
+        width: '80%',
+        backgroundColor: '#333333',
+        height: '10%',
+        paddingVertical: 10,
+        borderRadius: 20,
+        borderColor: 'green',
+        borderWidth: 1,
+        marginBottom: '30%'
+
     }
 });
